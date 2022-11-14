083e0293162f271a5340f7c95cae062660216d00
Asa <asourdiffe@gmail.com>
2022-11-14 08:37:37 -0800

    Add DZ60 v2 vial keymap (i.e. flex cut PCB). Only my layout currently - ANSI split space, split shift

---
+++ keyboards/dztech/dz60/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x14, 0x18, 0xB8, 0x23, 0xB3, 0x6C, 0x4A, 0xC9}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
